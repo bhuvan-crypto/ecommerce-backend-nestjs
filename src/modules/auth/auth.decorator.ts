baa73c19aaf1c096bc28c3e30368fd594ea07c89
bhuvan-crypto <55475746+bhuvan-crypto@users.noreply.github.com>
2025-11-17 12:02:03 +0000

    fixed import

--- src/modules/auth/auth.decorator.ts
+++ src/modules/auth/auth.decorator.ts
@@ -1,7 +1,7 @@
 import { applyDecorators, UseGuards } from '@nestjs/common';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { JwtAuthGuard } from '../auth/jwt-auth.guard';
-import { RolesGuard } from 'src/common/guards/roles.guard';
+import { RolesGuard } from '../../common/guards/roles.guard';
 
 export function Auth() {
   return applyDecorators(UseGuards(JwtAuthGuard, RolesGuard), ApiBearerAuth());
